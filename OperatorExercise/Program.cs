8005938c05f9887d33ccf847a9699e044b7059b3
Keyan117 <kdaneshvari98@gmail.com>
2023-03-30 17:27:41 -0700

    update

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,11 +6,11 @@
         {
             //For division, create two integer variables called a and b.
             var a = 17;
-            var b = 4
+            var b = 4;
             var quotient = a / b;
-            var remainder a % b;
+            var remainder = a % b;
 
-            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}")
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
 
             Console.WriteLine("Please enter the radius of your circle:");
             var userInput = Console.ReadLine();
@@ -24,11 +24,11 @@
         
         //define a method - which performs some functionality. that can
         //be used over and over again
-        Public static double AreaOfCircle(double radius)
+        Public Static (double radius);
         {
         //this is the scope of the method
         var area = Math.PI * Math.Pow (radius, 2);
 
-        return area:
+        Return area;
+        }
     }
-}
